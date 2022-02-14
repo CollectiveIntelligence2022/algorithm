aabb29456381acb8f67b7a18da77c917f9cc8cd7
ksh7628 <82318627+ksh7628@users.noreply.github.com>
2022-02-14 20:21:33 +0900

    20220208_김선홍

---
+++ 김선홍/Main_b1_1259_팰린드롬수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main_b1_1259_팰린드롬수 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			String str = br.readLine();
+			int size = str.length();
+
+			if (str.equals("0")) {
+				break;
+			}
+
+			boolean flag = true;
+			// �ռ��ڿ� �޼��ڸ� ���ʴ�� ���Ͽ� �߰��� ���ڰ� �ٸ��ٸ� �Ӹ���Ҽ��� �ƴ�
+			for (int i = 0; i < size / 2; i++) {
+				if (str.charAt(i) != str.charAt(size - 1 - i)) {
+					flag = false;
+					break;
+				}
+			}
+
+			if (flag) {
+				sb.append("yes\n");
+			} else {
+				sb.append("no\n");
+			}
+		}
+
+		System.out.print(sb);
+		br.close();
+	}
+}
